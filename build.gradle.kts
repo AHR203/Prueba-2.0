132e6d735405f945c5e3f57471796e96c9f81027
AHR203 <154491456+AHR203@users.noreply.github.com>
2024-09-16 18:41:42 -0500

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,3 +94,11 @@ subprojects {
 task<Delete>("clean") {
     delete(rootProject.buildDir)
 }
+
+	dependencyResolutionManagement {
+		repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+		repositories {
+			mavenCentral()
+			maven { url 'https://jitpack.io' }
+		}
+	}
